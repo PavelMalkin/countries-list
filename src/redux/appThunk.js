42e5c2f279c6319c39c78a77183d01527f28402d
PavelMalkin <56869959+PavelMalkin@users.noreply.github.com>
2020-12-12 20:08:09 +0200

    initial commit

---
+++ src/redux/appThunk.js
@@ -0,0 +1,8 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import axios from 'axios'
+import {toast} from "react-toastify";
+
+export const getCountriesList = createAsyncThunk('getCurrentWeather', () => {
+
+    return  null
+})
