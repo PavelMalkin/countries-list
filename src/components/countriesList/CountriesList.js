2cf022cc1c913feef3973f54178315dc7531eeaf
PavelMalkin <56869959+PavelMalkin@users.noreply.github.com>
2020-12-14 00:05:38 +0200

    feat add filter

--- src/components/countriesList/CountriesList.js
+++ src/components/countriesList/CountriesList.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useLocation} from "react-router-dom";
+
+import TextField from '@material-ui/core/TextField';
 
 import './CountriesList.scss'
 
@@ -8,14 +10,39 @@ import CountryRow from "../CountryRow/CountryRow";
 
 function CountriesList(props) {
     const location = useLocation();
-    const countriesList = (location.pathname === '/favorites')? props.favorites : props.countriesList
+    const [filter, setFilter] = useState('');
+    const countriesList = (location.pathname === '/favorites') ? props.favorites : props.countriesList
 
-    const rows = (props.hasFetched && countriesList.map(country =>
-            <CountryRow className="CountriesList__Country" key={country.numericCode} {...country}/>
-    ))
+    function searchName(value, string) {
+        let result = false;
+        const lowValue = value.toLowerCase();
+        const lowString = string.toLowerCase();
+        for (let i = 0; i <= string.length - value.length; i++) {
+            if (lowValue === lowString.slice(i, i + value.length)) {
+                result = true;
+                i = string.length;
+            }
+        }
+        return result
+    }
+
+    const rows = (props.hasFetched && countriesList.map(country => {
+        if (filter.length < 2 || searchName(filter, country.name)) {
+            return <CountryRow className="CountriesList__Country" key={country.numericCode} {...country}/>
+        }
+    }))
+
+    const handleFilter = (e) => {
+        setFilter(e.target.value)
+    }
 
     return (
         <div className='CountriesList'>
+            <TextField label="Filter country by name"
+                       variant="outlined"
+                       className="CountriesList__Search"
+                       value={filter}
+                       onChange={handleFilter}/>
             {rows}
         </div>
     );
